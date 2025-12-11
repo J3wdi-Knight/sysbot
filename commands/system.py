ac5c4739d11a98547439bd0260c21ffea94838cc
J3wDI <117535440+J3wdi-Knight@users.noreply.github.com>
2025-12-11 13:22:06 +0300

    .

--- commands/system.py
+++ commands/system.py
@@ -1,15 +1,15 @@
-import subprocess
-import os
+#import subprocess
+#import os
 
 # /screenshot
-def screenshot():
-    isExist = subprocess.run("ls ./screenshot.png", shell=True, capture_output=True, text=True)
-    if isExist == './screenshot.png\n':
-        os.system("rm -f './screenshot.png'")
+#def screenshot():
+#    isExist = subprocess.run("ls ./screenshot.png", shell=True, capture_output=True, text=True)
+#    if isExist == './screenshot.png\n':
+#        os.system("rm -f './screenshot.png'")
+#
+#    os.system("scrot screenshot.png")
 
-    os.system("scrot screenshot.png")
-
-    return "./screenshot.png"
+#    return "./screenshot.png"
 
 # /updates
 
